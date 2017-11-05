52a6cf4bda950fd494c7073ca4894718f924cd70
Kevin <dragn194@gmail.com>
2017-11-05 00:31:12 -0400

    Create Team model

--- VexRankings/app/models.py
+++ VexRankings/app/models.py
@@ -3,5 +3,9 @@ Definition of models.
 """
 
 from django.db import models
+import django.contrib.postgres.fields as pgfields
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=10)
+    elos = pgfields.ArrayField(base_field=models.FloatField())
+    elo_changes = pgfields.ArrayField(base_field=models.FloatField())
