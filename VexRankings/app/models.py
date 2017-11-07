3e3186719a204b014ff9311deaa19e138ea01970
Kevin <dragn194@gmail.com>
2017-11-07 17:43:01 -0500

    Set Team's name as its primary key

--- VexRankings/app/models.py
+++ VexRankings/app/models.py
@@ -6,6 +6,6 @@ from django.db import models
 import django.contrib.postgres.fields as pgfields
 
 class Team(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=10, primary_key=True)
     elos = pgfields.ArrayField(base_field=models.FloatField())
     elo_changes = pgfields.ArrayField(base_field=models.FloatField())
