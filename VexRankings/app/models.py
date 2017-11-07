5bfd41d5bce3c57f7071d041616e8308400d6b4e
Kevin <dragn194@gmail.com>
2017-11-07 17:43:16 -0500

    Add a method to calculate elo changes for a team

--- VexRankings/app/models.py
+++ VexRankings/app/models.py
@@ -9,3 +9,10 @@ class Team(models.Model):
     name = models.CharField(max_length=10, primary_key=True)
     elos = pgfields.ArrayField(base_field=models.FloatField())
     elo_changes = pgfields.ArrayField(base_field=models.FloatField())
+
+    def calculate_changes(self):
+        self.elo_changes = list()
+        #no change for the first week
+        self.elo_changes.append(0)
+        for i in range(1, len(self.elos)):
+            self.elo_changes.append(self.elos[i] - self.elos[i -1])
