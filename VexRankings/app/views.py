dab267ad10c247900c09c21e615f2736a59aee32
Kevin <dragn194@gmail.com>
2017-11-11 17:08:02 -0500

    Make get_rankings_data return a dict instead of a list

--- VexRankings/app/views.py
+++ VexRankings/app/views.py
@@ -17,6 +17,8 @@ def api_get_rankings_data(request):
     week_num = request.GET.get('week_num', None)
     if week_num == None:
         week_num = vexdb.get_num_weeks_to_today()
+    else:
+        week_num = int(week_num)
     ordered_teams = Team.objects.all().order_by('elos')
     rankings_data = list()
     for team in ordered_teams:
@@ -25,7 +27,7 @@ def api_get_rankings_data(request):
             'elo' : team.elos[week_num],
             'elo_change' : team.elo_changes[week_num]
             })
-    return JsonResponse(rankings_data, safe=False)
+    return JsonResponse({ 'data' : rankings_data })
 
 def rankings(request):
     return render(request, 'app/rankings.html')
