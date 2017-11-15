6035ecd14da7b793341d9650ddbaa293f7a0b8a6
Kevin <dragn194@gmail.com>
2017-11-14 21:49:38 -0500

    Add percent chance transitions
    Instead of odometer we're using a much simpler library that doesn't
    overwrite a bunch of styles.

--- VexRankings/app/static/app/rankings.js
+++ VexRankings/app/static/app/rankings.js
@@ -1,3 +1,27 @@
+animate_chance = function(selector, num) {
+    // how many decimal places allows
+    var decimal_places = 2;
+    var decimal_factor = decimal_places === 0 ? 1 : Math.pow(10, decimal_places);
+
+    selector
+      .animateNumber(
+        {
+          number: num * decimal_factor,
+
+          numberStep: function(now, tween) {
+            var floored_number = Math.floor(now) / decimal_factor,
+                target = $(tween.elem);
+
+            // force decimal places even if they are 0
+            floored_number = floored_number.toFixed(decimal_places);
+
+            target.text(floored_number + "%");
+          }
+        },
+        1000
+      );
+}
+
 $(document).ready(function() {
     var rankings_table = $("#rankings-table").DataTable({
         ajax : "/api/get_rankings",
@@ -22,8 +46,8 @@ $(document).ready(function() {
         e.preventDefault();
         week_idx = $("#week-select").val();
         $.get("/api/predict_match?" + $("#predict-form").serialize() + "&week_idx=" + week_idx, function (data) {
-            $(".red-chance").text(data.red_chance);
-            $(".blue-chance").text(data.blue_chance);
+            animate_chance($(".red-chance"), data.red_chance);
+            animate_chance($(".blue-chance"), data.blue_chance);
         });
     });
 });
